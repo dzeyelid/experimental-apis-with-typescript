c0aed63f22329ddb2d7f1457dc3e8fa1d19bf18b
Kazumi OHIRA <dzeyelid@gmail.com>
2018-12-13 17:45:03 +0900

    Created unit tests for controllers.

--- banking/src/balances/balances.controller.spec.ts
+++ banking/src/balances/balances.controller.spec.ts
@@ -1,16 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as nanoid from 'nanoid';
+import * as moment from 'moment';
 import { BalancesController } from './balances.controller';
+import { BalancesService } from './balances.service';
+import { Balance } from './interfaces/balance.interface';
+import { BalanceShowResponseBody } from './interfaces/balance-show-response-body.interface';
+import { BalanceCreateRequestBody } from './interfaces/balance-create-request-body.interface';
+import { BalanceCreateResponseBody } from './interfaces/balance-create-response-body.interface';
 
-describe('Balances Controller', () => {
-  let module: TestingModule;
+describe('BalancesController', () => {
+  let balancesController: BalancesController;
+  let balancesService: BalancesService;
   
-  beforeAll(async () => {
-    module = await Test.createTestingModule({
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [BalancesController],
+      providers: [BalancesService],
     }).compile();
+
+    balancesService = module.get<BalancesService>(BalancesService);
+    balancesController = module.get<BalancesController>(BalancesController);
   });
-  it('should be defined', () => {
-    const controller: BalancesController = module.get<BalancesController>(BalancesController);
-    expect(controller).toBeDefined();
+
+  describe('create', () => {
+    it('should pass parameters to BalancesService and return the result', async () => {
+      const balance: Balance = createNewBalance({amount: 10});
+      const param: BalanceCreateRequestBody = {
+        balanceId: balance.id,
+        amount: balance.amount
+      };
+      const expected: BalanceCreateResponseBody = {
+        balance: balance
+      };
+      jest.spyOn(balancesService, 'create').mockImplementation(() => balance);
+      expect(await balancesController.create(param)).toEqual(expected);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of Balance interface', async () => {
+      let result: Balance[] = [];
+      result.push(createNewBalance());
+      result.push(createNewBalance());
+      jest.spyOn(balancesService, 'findAll').mockImplementation(() => result);
+      expect(await balancesController.findAll()).toBe(result);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return an object BalanceShowResponseBody', async () => {
+      const balanceId = nanoid();
+      const balance: Balance = createNewBalance({id: balanceId});
+      const expected: BalanceShowResponseBody = { balance: balance };
+      jest.spyOn(balancesService, 'findOne').mockImplementation(() => {
+        return new Promise<Balance>((resolve) => resolve(balance))
+      });
+      expect(await balancesController.findOne(balanceId)).toEqual(expected);
+    });
   });
 });
+
+const createNewBalance = (param: {id?: number | string, amount?: number} = {}): Balance => {
+  return {
+    id: param.id || nanoid(),
+    amount: param.amount || 0,
+    createdAt: moment().toDate()
+  };
+}
--- banking/src/deposits/deposits.controller.spec.ts
+++ banking/src/deposits/deposits.controller.spec.ts
@@ -1,16 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as nanoid from 'nanoid';
+import * as moment from 'moment';
 import { DepositsController } from './deposits.controller';
+import { DepositsService } from './deposits.service';
+import { Deposit } from './interfaces/deposit.interface';
+import { DepositCreateRequestBody } from './interfaces/deposit-create-request-body.interface';
+import { DepositCreateResponseBody } from './interfaces/deposit-create-response-body.interface';
+import { DepositListResponseBody } from './interfaces/deposit-list-response-body.interface';
+import { DepositShowResponseBody } from './interfaces/deposit-show-response-body.interface';
+import { Balance } from '../balances/interfaces/balance.interface';
 
 describe('Deposits Controller', () => {
-  let module: TestingModule;
+  let depositsController: DepositsController;
+  let depositsService: DepositsService;
   
-  beforeAll(async () => {
-    module = await Test.createTestingModule({
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [DepositsController],
+      providers: [DepositsService],
     }).compile();
+
+    depositsController = module.get<DepositsController>(DepositsController);
+    depositsService = module.get<DepositsService>(DepositsService);
   });
-  it('should be defined', () => {
-    const controller: DepositsController = module.get<DepositsController>(DepositsController);
-    expect(controller).toBeDefined();
+
+  describe('create', () => {
+    it('should pass parameters to DepositsService and return the result', async () => {
+      const balance: Balance = createNewBalance({amount: 10});
+      const deposit: Deposit = createNewDeposit({
+        balanceId: balance.id.toString(),
+        amount: 3
+      });
+      const param: DepositCreateRequestBody = {
+        balanceId: balance.id,
+        amount: 5
+      };
+      const expected: DepositCreateResponseBody = {
+        deposit: deposit
+      };
+      jest.spyOn(depositsService, 'create').mockImplementation(() => deposit);
+      expect(await depositsController.create(param)).toEqual(expected);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an object DepositListResponseBody', async () => {
+      let deposits: Deposit[] = [];
+      deposits.push(createNewDeposit({balanceId: nanoid(), amount: 1}));
+      deposits.push(createNewDeposit({balanceId: nanoid(), amount: 2}));
+      const expected: DepositListResponseBody = {
+        deposits: deposits
+      }
+      jest.spyOn(depositsService, 'findAll').mockImplementation(() => deposits);
+      expect(await depositsController.findAll()).toEqual(expected);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return an object DepositShowResponseBody', async () => {
+      const balance: Balance = createNewBalance({amount: 10});
+      const deposit: Deposit = createNewDeposit({
+        balanceId: balance.id.toString(),
+        amount: 3
+      });
+      const expected: DepositShowResponseBody = {
+        deposit: deposit
+      };
+      jest.spyOn(depositsService, 'findOne').mockImplementation(() => deposit);
+      expect(await depositsController.findOne(deposit.id)).toEqual(expected);
+    });
   });
 });
+
+const createNewBalance = (param: {id?: number | string, amount?: number} = {}): Balance => {
+  return {
+    id: param.id || nanoid(),
+    amount: param.amount || 0,
+    createdAt: moment().toDate()
+  };
+}
+
+const createNewDeposit = (param: {balanceId: string, amount: number}): Deposit => {
+  return {
+    id: nanoid(),
+    balanceId: param.balanceId,
+    amount: param.amount,
+    createdAt: moment().toDate()
+  };
+}
